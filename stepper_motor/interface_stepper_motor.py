8886e3ca5c078d37122f3be43298ec0cde56f369
Ritik Mishra <ritik.mishra314@gmail.com>
2019-12-23 10:58:12 -0600

    Add a reset method corresponding to A4988L

--- stepper_motor/interface_stepper_motor.py
+++ stepper_motor/interface_stepper_motor.py
@@ -17,6 +17,7 @@ class StepperMotor(ABC):
     def step(self, forwards: bool = True):
         """
         Rotate the stepper motor by 1 step. Direction indicated by `forwards`.
+        Near instant.
 
         :param forwards: if we should rotate forwards or backwards
         :return: None
@@ -29,6 +30,16 @@ class StepperMotor(ABC):
         Returns the actual number of steps this motor has traveled.
 
         :return: position of motor shaft
+        """
+        pass
+
+    @abstractmethod
+    def reset_pos(self, new_pos_val = 0):
+        """
+        Reset the measured pose based on other sensor input e.g a limit switch
+
+        :param new_pos_val: The new recorded current position. Default: 0
+        :return: None
         """
         pass
 
