c3d9b5a2ac0d612a4955ff9a27a40b84b3f06aa3
Ritik Mishra <ritik.mishra314@gmail.com>
2019-12-25 23:18:54 -0600

    Fix bug in run_to_angle; was confusing revs with steps

--- stepper_motor/stepper_motor_wrapper.py
+++ stepper_motor/stepper_motor_wrapper.py
@@ -13,27 +13,16 @@ class StepperMotorWrapper(object):
         self.output_radians_over_input_revs = gear_reduction * 2 * math.pi
 
     def get_pos(self) -> Radians:
-        """
-        :return: Get the angular position of the stepper motor in radians
-        """
         return self._motor.get_revs() * self.output_radians_over_input_revs
 
-    def run_to_angle(self, target_radians: Radians, target_radians_per_sec):
-        """
-        Drive the stepper motor to a location in the preferred units
-        :param target_radians:
-        :param target_radians_per_sec:
-        :return:
-        """
+    def run_to_angle(self, target_radians: Radians, target_output_rpm):
         target_pos_revs = target_radians / self.output_radians_over_input_revs
-        target_vel_rpm = target_radians_per_sec * 60 / self.output_radians_over_input_revs
+        target_pos_steps = target_pos_revs * self._motor.steps_per_rev
+        target_motor_rpm = target_output_rpm * math.tau / self.output_radians_over_input_revs
 
-        return self._motor.run_to_pos(target_pos_revs, target_vel_rpm)
+        self._motor.run_to_pos(target_pos_steps, target_motor_rpm)
+
+        return self.get_pos()
 
     def reset_pos(self, new_val_radians: Radians = 0):
-        """
-        Override the read value
-        :param new_val_radians:
-        :return:
-        """
         self._motor.reset_pos(new_val_radians / self.output_radians_over_input_revs)
